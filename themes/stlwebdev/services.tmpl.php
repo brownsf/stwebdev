0d3c13d0992b83c1d3b504b188a0f73e7559b73c
Scott Brown <sbrown@nucloudglobal.com>
2015-05-31 14:59:17 -0500

    service styling

--- themes/stlwebdev/services.tmpl.php
+++ themes/stlwebdev/services.tmpl.php
@@ -20,57 +20,56 @@ get_header();
                     </div>
 
                 </div>
-
                 <div class="row">
-                    <div class="span9">
+                    <div class="col-md-9">
+                        <ul class="nav nav-tabs" role="tablist" id="myTab">
 
-                                <ul class="nav nav-tabs" role="tablist" id="myTab">
+                            <?php
+                            $args = array('posts_per_page' => -1, 'post_type' => 'my_services', 'orderby' => 'menu_order');
+                            $firstTitles = 'active';
+                            $myposts = get_posts($args);
+                            foreach ($myposts as $post) : setup_postdata($post); ?>
+                                <li role="presentation" class="<?php echo $firstTitles ?>">
+                                    <a aria-controls="home" role="tab" data-toggle="tab"
+                                       data-id="<?php echo $post->ID ?>"
+                                       href="#service-<?php echo $post->ID ?>"><?php the_title(); ?></a>
+                                </li>
 
-                                    <?php
-                                    $args = array('posts_per_page' => -1, 'post_type' => 'my_services', 'orderby' => 'menu_order');
-                                    $firstTitles = 'active';
-                                    $myposts = get_posts($args);
-                                    foreach ($myposts as $post) : setup_postdata($post); ?>
-                                        <li role="presentation" class="<?php echo $firstTitles ?>">
-                                            <a aria-controls="home" role="tab" data-toggle="tab"
-                                               data-id="<?php echo $post->ID ?>"
-                                               href="#service-<?php echo $post->ID ?>"><?php the_title(); ?></a>
-                                        </li>
+                                <?php
+                                $firstTitles = '';
+                            endforeach;
+                            wp_reset_postdata();
+                            ?>
 
-                                        <?php
-                                        $firstTitles = '';
-                                    endforeach;
-                                    wp_reset_postdata();
-                                    ?>
-
-                                </ul>
+                        </ul>
 
 
-                                <div class="tab-content" id="service-content">
-                                    <?php
-                                    $first = 'active';
-                                    $args = array('post_type' => 'my_services', 'posts_per_page' => 10, 'orderby' => 'menu_order');
-                                    $loop = new WP_Query($args);
-                                    while ($loop->have_posts()) : $loop->the_post();
-                                        echo '<div role="tabpanel" class="tab-pane ' . $first . '" id="service-' . $post->ID . '" class="row">';
-                                        the_title('<h3>', '</h3>');
-                                        echo '<div class="entry-content">';
-                                        the_content();
-                                        echo '</div>';
-                                        echo '</div>';
-                                        $first = '';
-                                    endwhile;
+                        <div class="tab-content" id="service-content">
+                            <?php
+                            $first = 'active';
+                            $args = array('post_type' => 'my_services', 'posts_per_page' => 10, 'orderby' => 'menu_order');
+                            $loop = new WP_Query($args);
+                            while ($loop->have_posts()) : $loop->the_post();
+                                echo '<div role="tabpanel" class="tab-pane ' . $first . '" id="service-' . $post->ID . '" class="row">';
+                                the_title('<h3>', '</h3>');
+                                echo '<div class="entry-content">';
+                                the_content();
+                                echo '</div>';
+                                echo '</div>';
+                                $first = '';
+                            endwhile;
 
-                                    ?>
-                                </div>
-                            </div>
+                            ?>
                         </div>
                     </div>
+                    <?php get_sidebar('content'); ?>
+                </div>
+            </div>
 
             <!-- #content -->
         </div>
         <!-- #primary -->
-
+        <?php get_sidebar('content'); ?>
     </div><!-- #main-content -->
     <script>
 
@@ -84,4 +83,5 @@ get_header();
 
     </script>
 <?php
+get_sidebar();
 get_footer();
