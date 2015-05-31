11cbbcac33031a40629731c8e25b4fa581d3ba3c
Scott Brown <sbrown@nucloudglobal.com>
2015-05-31 17:17:04 -0500

    single fix

--- themes/stlwebdev/index.php
+++ themes/stlwebdev/index.php
@@ -8,22 +8,22 @@ get_header(); ?>
     <div id="main-content" class="main-content container">
 
         <?php
-        if ( is_front_page() ) {
+        if (is_front_page()) {
             // Include the featured content template.
-            get_template_part( 'featured-content' );
+            get_template_part('featured-content');
         }
         ?>
 
-        <div id="primary" class="content-area">
-            <div id="content" class="site-content" role="main">
+        <div id="primary" class="content-area row">
+            <div id="content" class="site-content col-md-9" role="main">
 
                 <?php
-                if ( have_posts() ) :
+                if (have_posts()) :
                     // Start the Loop.
-                    while ( have_posts() ) : the_post();
-                        ?> <h3><a href="<?php echo get_the_permalink()?>"><?php echo get_the_title()?></a> </h3>
+                    while (have_posts()) : the_post();
+                        ?> <h3><a href="<?php echo get_the_permalink() ?>"><?php echo get_the_title() ?></a></h3>
                         <?php
-                    the_content();
+                        the_content();
 
 
                     endwhile;
@@ -36,9 +36,11 @@ get_header(); ?>
                 endif;
                 ?>
 
-            </div><!-- #content -->
-        </div><!-- #primary -->
-        <?php get_sidebar( 'content' ); ?>
+            </div>
+            <!-- #content -->
+            <!-- #primary -->
+            <?php get_sidebar('content'); ?>
+        </div>
     </div><!-- #main-content -->
 
 <?php
